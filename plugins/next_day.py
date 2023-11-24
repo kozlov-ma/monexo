9e974decb17357cb7b7c763a6a5ac7ce6e2972ac
Mikhail Kozlov <kozlov_ma+nespamte@icloud.com>
2023-11-24 14:04:39 +0500

    Update all users every 24 hours

--- plugins/next_day.py
+++ plugins/next_day.py
@@ -1,29 +1,52 @@
-from telethon import events
+import asyncio
+
+import loguru
+from telethon import events, TelegramClient
 from telethon.tl.custom import Message
 from telethon.tl.types import User as TgUser
+from domain import User
 
 from pkg import state
 from plugins import stats
 
 
-async def init(bot):
+async def init(bot: TelegramClient):
     @bot.on(events.NewMessage(pattern="/nextday"))
     async def next_day(event: Message) -> None:
         sender: TgUser = await event.get_sender()
 
-        old_user = await state.get().users_repo.get_by_id(sender.id)
-        if old_user is None:
+        user = await state.get().users_repo.get_by_id(sender.id)
+        if user is None:
             await event.respond(
                 "Сначала введите укажите срок и бюджет с помощью /start"
             )
             return
 
-        await stats.stats(event)
+        await next_day_for(bot, user)
 
-        user = old_user.apply_today()
-        await state.get().users_repo.update_user(user)
+    asyncio.create_task(update_users_periodically(bot, 24 * 60 * 60))
 
-        # FIXME: не отправлять это сообщение если срок кончился
-        await event.respond(
-            f"Начался новый день. Бюджет на сегодня: **{user.budget_today}**"
-        )
+
+async def update_users_periodically(bot: TelegramClient, duration_secs: float) -> None:
+    await asyncio.sleep(duration_secs)
+    tasks = []
+    for user in await state.get().users_repo.get_all():
+        tasks.append(next_day_for(bot, user))
+
+    await asyncio.gather(*tasks)
+
+    loguru.logger.info(f"Updated all users, next update in {duration_secs} seconds.")
+
+
+async def next_day_for(bot: TelegramClient, user: User) -> None:
+    tg_user = TgUser(user.id)
+
+    await stats.send_stats(bot, user)
+
+    user = user.apply_today()
+    await state.get().users_repo.update_user(user)
+
+    # FIXME: не отправлять это сообщение если срок кончился
+    await bot.send_message(
+        tg_user, f"Начался новый день. Бюджет на сегодня: **{user.budget_today}**"
+    )
--- plugins/stats.py
+++ plugins/stats.py
@@ -1,4 +1,4 @@
-from telethon import events
+from telethon import events, TelegramClient
 from telethon.tl.custom import Message
 from telethon.tl.types import User as TgUser
 
@@ -7,18 +7,23 @@ from pkg import state
 
 
 async def init(bot):
-    bot.on(events.NewMessage(pattern="/stats"))(stats)
+    @bot.on(events.NewMessage(pattern="/stats"))
+    async def stats(event: Message) -> None:
+        sender: TgUser = await event.get_sender()
+
+        user = await state.get().users_repo.get_by_id(sender.id)
+        if user is None:
+            await event.respond(
+                "Сначала введите укажите срок и бюджет с помощью /start"
+            )
+            return
+
+        await send_stats(bot, user)
 
 
-async def stats(event: Message) -> None:
-    sender: TgUser = await event.get_sender()
-
-    user = await state.get().users_repo.get_by_id(sender.id)
-    if user is None:
-        await event.respond("Сначала введите укажите срок и бюджет с помощью /start")
-        return
-
-    await event.respond(stats_for_today(user))
+async def send_stats(bot: TelegramClient, user: User) -> None:
+    tg_user = TgUser(user.id)
+    await bot.send_message(tg_user, stats_for_today(user))
 
 
 def stats_for_today(user: User) -> str:
