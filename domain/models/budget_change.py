7d07c478e9585b34dd9084482f0e13e2d3537963
Alexander Sapegin <sapegin04@mail.ru>
2023-12-20 20:07:10 +0500

    Fix 3.0

--- domain/models/budget_change.py
+++ domain/models/budget_change.py
@@ -4,7 +4,7 @@ import typing
 from dataclasses import dataclass, replace
 
 from sqlalchemy import Float, Integer, ForeignKey, String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, N
 
 from domain.models.db_base import Base
 
@@ -15,6 +15,7 @@ class BudgetChange:
 
     user_id: int
     category_id: int
+    message_id: int
 
     value: float
     is_income: bool
@@ -26,19 +27,21 @@ class DbBudgetChange(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
 
     user_telegram_id: Mapped[int] = mapped_column(Integer)
-    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
+    category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=True)
+    message_telegram_id: Mapped[int] = mapped_column(Integer)
 
     value: Mapped[float] = mapped_column(Float)
     is_income: Mapped[bool] = mapped_column(Boolean)
 
     def to_budget_change(self) -> BudgetChange:
-        return BudgetChange(self.id, self.user_telegram_id, self.category_id, self.value, self.is_income)
+        return BudgetChange(self.id, self.user_telegram_id, self.category_id,
+                            self.message_telegram_id, self.value, self.is_income)
 
     @staticmethod
     def from_budget_change(budget_change: BudgetChange) -> DbBudgetChange:
         return DbBudgetChange(id=budget_change.id, user_telegram_id=budget_change.user_id,
-                              category_id=budget_change.category_id, value=budget_change.value,
-                              is_income=budget_change.is_income)
+                              category_id=budget_change.category_id, message_telegram_id=budget_change.message_id,
+                              value=budget_change.value, is_income=budget_change.is_income)
 
     def __repr__(self):
         return \
