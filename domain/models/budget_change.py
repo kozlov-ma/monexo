074fba91437f2962f8f85244310a05eb9f6b16ca
Alexander Sapegin <sapegin04@mail.ru>
2024-01-03 20:50:05 +0500

    Check this commit, please

--- domain/models/budget_change.py
+++ domain/models/budget_change.py
@@ -18,7 +18,6 @@ class BudgetChange:
     message_id: int
 
     value: float
-    is_income: bool
 
 
 class DbBudgetChange(Base):
@@ -31,19 +30,18 @@ class DbBudgetChange(Base):
     message_telegram_id: Mapped[int] = mapped_column(Integer)
 
     value: Mapped[float] = mapped_column(Float)
-    is_income: Mapped[bool] = mapped_column(Boolean)
     
     def to_budget_change(self) -> BudgetChange:
         return BudgetChange(self.id, self.user_telegram_id, self.category_id,
-                            self.message_telegram_id, self.value, self.is_income)
+                            self.message_telegram_id, self.value)
 
     @staticmethod
     def from_budget_change(budget_change: BudgetChange) -> DbBudgetChange:
         return DbBudgetChange(id=budget_change.id, user_telegram_id=budget_change.user_id,
                               category_id=budget_change.category_id, message_telegram_id=budget_change.message_id,
-                              value=budget_change.value, is_income=budget_change.is_income)
+                              value=budget_change.value)
 
     def __repr__(self):
         return \
-            f'<Category id={self.id} user_telegram_id={self.user_telegram_id} category_id={
-            self.category_id} value={self.value} is_income={self.is_income}>'
+            f'<BudgetChange id={self.id} user_telegram_id={self.user_telegram_id} category_id={
+            self.category_id} message_telegram_id={self.message_telegram_id} value={self.value}>'
