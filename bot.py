1e1eb116e7f7fb39bed305119ba4a03830954711
hairysnake <mamaev.gosha99@gmail.com>
2024-01-05 01:44:51 +0500

    timezones add and minimally tested

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import asyncio
 import logging
 from os import getenv
 
+
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from dotenv import load_dotenv
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 import app
 import bot_modules
@@ -18,7 +21,9 @@ from bot_modules.toggle_autoupdate import autoupdate_router
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 
-from datetime import datetime
+
+
+from datetime import datetime, timedelta
 import time
 from pytz import timezone
 
@@ -33,20 +38,10 @@ TOKEN = getenv("BOT_TOKEN")
 POSTGRES_PASSWORD = getenv("POSTGRES_PASSWORD")
 
 
-async def schedule_cycle():
-    while True:
-        if datetime.now().second == 0:
-            break
-    while True:
-        await update_group_of_users()
-        await asyncio.sleep(60 - datetime.now().second)
-
-
 async def update_group_of_users():
     users = await app.state.get().tz_repo.get_all()
     for user in users:
-        # datetime.now(timezone('Europe/Moscow')).second + user.timezone % 24 == 0 and
-        if user.is_updatable:
+        if datetime.now(timezone('Europe/Moscow')).hour + user.timezone == 0 and user.is_updatable:
             await bot_modules.next_day._next_day(user.user_id)
 
 
@@ -72,13 +67,19 @@ async def main() -> None:
         stats_router
     )
 
+    now = datetime.now()
+    next_hour = now.replace(microsecond=0, second=0, minute=0) + timedelta(hours=1)
+
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(update_group_of_users, "interval", hours=1, start_date=next_hour)
 
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
 
     bot_modules.set_bot(bot)
-    await asyncio.create_task(dp.start_polling(bot))
-    await asyncio.create_task(schedule_cycle())
+
+    scheduler.start()
+    await dp.start_polling(bot, skip_updates=False)
 
 
 if __name__ == "__main__":
